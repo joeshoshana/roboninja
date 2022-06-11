b886690fb7cb5f583bb55110f845b359f314cea5
joeshoshana <joeshoshana@gmail.com>
2022-06-11 13:33:47 +0300

    k

--- robomaster_sim/UI/launch.sh
+++ robomaster_sim/UI/launch.sh
@@ -1,3 +1,4 @@
 source /opt/ros/noetic/setup.bash
-. ../devel/setup.bash
+cd ..
+. devel/setup.bash
 roslaunch turtlebot3_gazebo turtlebot3_house.launch
--- robomaster_sim/UI/teleop.sh
+++ robomaster_sim/UI/teleop.sh
@@ -1,3 +1,4 @@
 source /opt/ros/noetic/setup.bash
-. ../devel/setup.bash
+cd ..
+. devel/setup.bash
 rosrun teleop_twist_keyboard teleop_twist_keyboard.py 
